64e45ead14f7bf1ab0b00657f199a8201389f176
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2025-06-16 12:22:16 -0700

    feat: agregar una validacion de conexión a la base de datos utilizando Prisma

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,16 @@
 import app from '@/server/index.js'
+import { prisma } from '@/config/db.js'
 
 const port = process.env.PORT || 3000
+
+//db connection
+prisma.$connect()
+  .then(() => {
+    console.log('Database connected successfully')
+  })
+  .catch((error) => {
+    console.error('Database connection failed:', error)
+  })
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
